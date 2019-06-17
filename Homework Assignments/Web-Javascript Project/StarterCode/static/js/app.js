d5aad4e60f45c5fd3350e6bec409395cc137374d
Ryan Schraeder <rschraeder2@gmail.com>
2019-06-17 15:55:01 -0600

    Update app.js
    Fixed button loading filter for date
    Still need to add filters for Country, State, City, Shape

--- Homework Assignments/Web-Javascript Project/StarterCode/static/js/app.js
+++ Homework Assignments/Web-Javascript Project/StarterCode/static/js/app.js
@@ -1,26 +1,78 @@
 // from data.js
 var tableData = data;
 var buttonFilter = d3.select("#filter-btn");
-var table = d3.select("tbody");  
+var table = d3.select("tbody");
 var inputState = d3.select("#state");
 var inputCountry = d3.select("#country");
-var inputShape = d3.select("#shape"); 
-var inputDate = d3.select("#datetime"); 
+var inputShape = d3.select("#shape");
+var inputDate = d3.select("#datetime");
 var inputCity = d3.select("#city");
+var dateValue
 
 // Loop through data objects
-tableData.forEach(function(sighting) {
-    console.log(sighting) 
-}); 
+// tableData.forEach(function(sighting) {
+//     console.log(sighting) 
+// }); 
+
+console.log(tableData);
 
 // Append data to table 
-tableData.forEach((sighting) => {
-    var row = table.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+// tableData.forEach((sighting) => {
+//     var row = table.append("tr");
+//     Object.entries(sighting).forEach(([key, value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
+
+function insertdata(array) {
+    table.html("");
+    array.forEach((sighting) => {
+        var row = table.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+};
 
 //On Button click, filter input 
-//buttonFilter.on("click",
+
+buttonFilter.on("click", function () {
+
+    d3.event.preventDefault();
+
+    // Get the value property of the input element
+    var dateValue = inputDate.property("value");
+    //    var stateValue = inputState.property("value");
+
+    // Prevent the whole page from refreshing.
+    console.log(" Input -- Entry ")
+    console.log("dateValue  " + dateValue);
+    //    console.log("stateValue " + stateValue);
+    console.log(" Processing date/value entry ")
+    filterData = tableData;
+
+    //    var filterData = tableData.filter(filterData => (filterData.datetime == dateValue) && (filterData.state == stateValue));
+    var filterData = tableData.filter(paramData => (paramData.datetime == dateValue));
+
+    console.log(filterData)
+    // loadFiltData = "y";
+    // if (loadFiltData == 'y') {
+    //     console.log("input filter data 'y'")
+    //     finalData = filterData;
+    // }
+    // else {
+    //     console.log("input filter data 'n'")
+    //     finalData = tableData;
+    // }
+
+    //Clear all previous data from UFO table to isolate search results
+    //    inputTbody.html("");
+
+    console.log(' Loading Filter Data ');
+
+    insertdata(filterData);
+});
+console.log(' First time load ');
+insertdata(tableData)
